fc3d3047d08e2a2614fc60e59ef89a5d2342994d
A. Farhan <arshadfarhan005@gmail.com>
2026-03-05 03:54:07 +0530

    Delete Skill Tracker (nexus)/my-backend directory

--- Skill Tracker (nexus)/my-backend/prisma/migrations/20260304053522_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Skill" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Skill_pkey" PRIMARY KEY ("id")
-);
